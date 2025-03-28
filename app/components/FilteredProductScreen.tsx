66b336707e99dde6532ff21a8e030331095eaadd
Jaddikhan <114812094+Jaddikhan@users.noreply.github.com>
2025-03-28 20:01:22 +0500

    edition in filtered Products components

--- app/components/FilteredProductScreen.tsx
+++ app/components/FilteredProductScreen.tsx
@@ -53,6 +53,7 @@ export default function FilteredProductScreen() {
   const deleteItem = async (id: string) => {
     try {
       await deleteDoc(doc(db, "items", id)); // Delete from Firestore
+      Alert.alert("Deleted Successfully are you want ?")
     } catch (error) {
       console.error("Error deleting item:", error);
       Alert.alert("Error", "Failed to delete item.");
